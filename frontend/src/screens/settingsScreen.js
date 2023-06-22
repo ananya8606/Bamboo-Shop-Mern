4c5499f8d62c3757cbe2d552dbdb7b76deddabc4
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2023-06-22 12:49:52 +0530

    Update settingsScreen.js

--- frontend/src/screens/settingsScreen.js
+++ frontend/src/screens/settingsScreen.js
@@ -28,7 +28,7 @@ const SettingsScreen = () => {
     
    
   const submitHandler = async (e) => {
-    
+     e.preventDefault();
     try {
       // Make an API request to update the settings
       const response = await fetch("/api/settings", {
