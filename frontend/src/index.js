83809838ab0504238540c013e583c7a3193fe36a
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2023-06-24 19:08:32 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,10 @@ import store from './store'
 import App from './App'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
+/*The rendered content is wrapped with the Provider component from the react-redux library. The Provider component 
+makes the Redux store available to all components in the application.
+Inside the Provider component, the Suspense component is used. The Suspense component is a built-in component in
+React that allows you to show a fallback content while waiting for asynchronous code to load. In this case, the fallback content is set to "...loading".*/
 ReactDOM.render(
   <Provider store={store}>
   <Suspense fallback="...loading">
@@ -16,3 +20,22 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
+/*In JavaScript, the `...` (three dots) is called the spread syntax or the rest syntax, depending on how it is used. Its behavior varies depending on the context in which it is used.
+
+1. Spread Syntax:
+   - When used in front of an iterable (such as an array or a string), it expands or spreads the elements of the iterable into individual elements.
+   - It is commonly used to create a new array or object by combining existing arrays or objects with additional elements or properties.
+   - Example usage:
+     const numbers = [1, 2, 3];
+     const newNumbers = [...numbers, 4, 5]; // [1, 2, 3, 4, 5]
+
+2. Rest Syntax:
+   - When used in a function declaration or function expression, it allows a variable to accept multiple arguments as an array. It collects the remaining arguments into an array.
+   - It is commonly used when the number of arguments passed to a function is unknown or can vary.
+   - Example usage:
+     function sum(...numbers) {
+       return numbers.reduce((total, num) => total + num, 0);
+     }
+
+     console.log(sum(1, 2, 3, 4, 5)); // Output: 15 */
+
