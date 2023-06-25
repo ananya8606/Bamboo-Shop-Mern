9adc49b3c557864c8f534951c276301036dc45ab
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2023-06-25 18:36:51 +0530

    Update addDecimals.js

--- frontend/src/Utils/addDecimals.js
+++ frontend/src/Utils/addDecimals.js
@@ -1,3 +1,4 @@
 export const addDecimals = (num) => {
   return (Math.round(num * 100) / 100).toFixed(2)
 }
+/* it converts a number with more than 2 decimal places to number with exactly 2 decimal place */
