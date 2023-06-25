a196a49f0c39b13f5aa906c4c08a69f665092476
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2023-06-25 16:38:52 +0530

    Update cartReducers.js

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -15,7 +15,16 @@ import {
         const item = action.payload
   
         const existItem = state.cartItems.find((x) => x.product === item.product)
-  
+            
+  /*...state: The spread operator (...) is used to create a shallow copy of the current state object. This ensures that the original state is not mutated directly.
+cartItems: state.cartItems.map((x) => x.product === existItem.product ? item : x): In this line, the cartItems array is updated. 
+The map function is used to iterate through each item in the cartItems array.
+For each item x in the cartItems array:
+The condition x.product === existItem.product checks if the product of the current item x matches the product of the existing item in the cart (existItem).
+If the condition is true, it means that the item being added is already present in the cart. In this case, the new item (item) is used instead of the existing item x in the updated array.
+If the condition is false, it means that the current item x is different from the existing item. In this case, the current item x is kept as it is in the updated array.
+The updated cartItems array is included in the returned object.*/
+
         if (existItem) {
           return {
             ...state,
